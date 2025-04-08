9893efd8219659efaa173e3843e5e15f47ea8bd4
Zhubeen Vakilzadeh <69344773+ZhubeenV@users.noreply.github.com>
2025-04-08 00:30:23 -0700

    further changes to rotary encoder
    Works more accurately now.
    Prints the output to the serial monitor

--- TestIrrigation/TestIrrigation.ino
+++ TestIrrigation/TestIrrigation.ino
@@ -1,14 +1,19 @@
 #define CLK 2
 #define DT 3
 #define SW 4
+#define PWM_PIN 9
 
 int lastCLKState;
 int currentCLKState;
+bool fanEn = false;
+int fanPWM = 0;
+char buffer[50];
 
 void setup(){
   pinMode(CLK, INPUT);
   pinMode(DT, INPUT);
   pinMode(SW, INPUT_PULLUP);
+  pinMode(PWM_PIN, OUTPUT); // Set PWM pin as output
   Serial.begin(9600);
   lastCLKState = digitalRead(CLK);
 }
@@ -18,16 +23,37 @@ void loop(){
 
   if (currentCLKState != lastCLKState){
     if(digitalRead(DT) != currentCLKState){
-      Serial.println("Rotated CCW");
+      
+      if (fanPWM > 0){
+        fanPWM -= 20;
+      } else{
+        fanPWM = 0;
+      }
+      sprintf(buffer, "Rotated CCW, %d\n", fanPWM);
+      Serial.print(buffer);
     }
     else{
-      Serial.println("Rotated CW");
+//      Serial.printf("Rotated CW");
+      if (fanPWM < 255){
+        fanPWM += 20;
+      } else {
+        fanPWM = 255;
+      }
+      sprintf(buffer, "Rotated CW, %d\n", fanPWM);
+      Serial.print(buffer);
     }
     }
     lastCLKState = currentCLKState;
     if (digitalRead(SW) == LOW) {
-        Serial.println("Button Pressed");
+        sprintf(buffer, "Fan enable status: %b\n", fanEn);
+        Serial.println(buffer);
+        fanEn != fanEn;
         delay(200);  // Debounce delay
   }
+  if(fanEn){
+    analogWrite(PWM_PIN, fanPWM);
+  }
+  else
+    analogWrite(PWM_PIN, 0);
   
 }
